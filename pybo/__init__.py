4b5e6022d6fd7778dc8dcd963857fce12859a2a4
kim joon <rlawns0604122@gmail.com>
2024-04-20 10:16:14 +0900

    0420 첫번쨰 파일 추가

---
+++ pybo/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+import config
+
+import os
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    db_path = os.path.join(base_dir, 'pybo.db')
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
+
+    #app.config.from_object(config)
+
+    #ORM 연결 메서드
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
+    from . import models
+    from .views import main_views, question_views,answer_views
+
+    app.register_blueprint(main_views.bp)
+    app.register_blueprint(question_views.bp)
+    app.register_blueprint(answer_views.bp)
+
+    return app
+
